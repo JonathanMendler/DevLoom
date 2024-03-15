947d687a282b5848e7f2f39550594594ef090073
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-15 10:17:16 -0500

    Added PostsIndex and map thru array

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,19 @@
 import { PostsIndex } from "./PostsIndex";
 
 export function Content() {
+  const posts = [
+    { id: 1, date: Date.today, name: "Deep JSX", content: "Let's dive into some advanced JSX!" },
+    {
+      id: 2,
+      date: Date.today,
+      name: "The Woes of an intermediate dev.",
+      content: "The emotional yo-yo'ing from superstar to imposter can weigh quite heavily.",
+    },
+  ];
+
   return (
     <div>
-      <PostsIndex />
+      <PostsIndex posts={posts} />
     </div>
   );
 }
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,7 +1,14 @@
-export function PostsIndex() {
+export function PostsIndex(props) {
   return (
     <div>
       <h1>All Posts</h1>
+      {props.posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.name}</h2>
+          <h3>{post.date}</h3>
+          <p>{post.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
