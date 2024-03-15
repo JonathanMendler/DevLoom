30afc1e051be14af9dd2026102673ae028bab98e
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-15 10:35:45 -0500

    Add useState and useEffect

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,15 +1,29 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { PostsIndex } from "./PostsIndex";
 
 export function Content() {
-  const posts = [
-    { id: 1, date: Date.today, name: "Deep JSX", content: "Let's dive into some advanced JSX!" },
-    {
-      id: 2,
-      date: Date.today,
-      name: "The Woes of an intermediate dev.",
-      content: "The emotional yo-yo'ing from superstar to imposter can weigh quite heavily.",
-    },
-  ];
+  // const posts = [
+  //   { id: 1, date: Date.today, name: "Deep JSX", content: "Let's dive into some advanced JSX!" },
+  //   {
+  //     id: 2,
+  //     date: Date.today,
+  //     name: "The Woes of an intermediate dev.",
+  //     content: "The emotional yo-yo'ing from superstar to imposter can weigh quite heavily.",
+  //   },
+  // ];
+
+  const [posts, setPosts] = useState([]);
+
+  const handleIndexPosts = () => {
+    console.log("handleIndexPosts");
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+      setPosts(response.data);
+    });
+  };
+
+  useEffect(handleIndexPosts, []);
 
   return (
     <div>
