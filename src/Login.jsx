1cace906fffd6f29b954a8d24486c187a80c2d78
Jonathan Mendler <jon.mendler@gmail.com>
2024-03-26 22:05:09 -0500

    Errors in Signup,Login

--- src/Login.jsx
+++ src/Login.jsx
@@ -33,7 +33,7 @@ export function Login() {
       <h1>Login</h1>
       <ul>
         {errors.map((error) => (
-          <li key={error}>(error</li>
+          <li key={error}>{error}</li>
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -25,22 +25,25 @@ export function Signup() {
     <div id="signup">
       <h1>Signup</h1>
       <ul>
-        <form onSubmit={handleSubmit}>
-          <div>
-            Name: <input name="name" type="text" />
-          </div>
-          <div>
-            Email: <input name="email" type="email" />
-          </div>
-          <div>
-            Password: <input name="password" type="password" />
-          </div>
-          <div>
-            Password confirmation: <input name="password_confirmation" type="password" />
-          </div>
-          <button type="submit">Signup</button>
-        </form>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input name="name" type="text" />
+        </div>
+        <div>
+          Email: <input name="email" type="email" />
+        </div>
+        <div>
+          Password: <input name="password" type="password" />
+        </div>
+        <div>
+          Password confirmation: <input name="password_confirmation" type="password" />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
     </div>
   );
 }
